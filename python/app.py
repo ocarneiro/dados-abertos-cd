ff2800add6b34212986b14160d86b826f8b857c8
Otavio Carneiro <otaviocsantos@gmail.com>
2014-06-19 20:11:27 -0300

    Bootstrap collapse - esconde a api e mostra sob demanda

--- python/app.py
+++ python/app.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template
+from suds.client import Client  #importa o cliente
+
+#suds - consulta ao webservice
+url = 'http://www.camara.gov.br/SitCamaraWS/Proposicoes.asmx?wsdl'
+client = Client(url, headers={'Content-Type': 'text/xml; charset=UTF-8'})
+ano = 2013
+
+def consulta() :
+#print (client)
+    resultado = client.service.ListarProposicoes(\
+        sigla='MPV', numero=None, ano=ano, \
+        datApresentacaoIni=None, datApresentacaoFim=None, \
+        idTipoAutor=None, parteNomeAutor=None, \
+        siglaPartidoAutor=None, siglaUFAutor=None, \
+        generoAutor=None, \
+        codEstado=None, codOrgaoEstado=None, \
+        emTramitacao=1) 
+    return resultado
+
+#permite preservar as quebras de linha
+def nl2br(value): 
+     return value.replace('\n','<br>\n')
+#jinja_env.filters['nl2br'] = nl2br
 
 #dados utilizados pela página
 app_data = {}
@@ -12,6 +35,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     app_data['isso'] = 'aquilo'
+    app_data['cliente'] = client
+    resultado = consulta()
+    app_data['tamanho'] = len(resultado['proposicoes']['proposicao'])
     return render_template('welcome.html', data=app_data)
 
 # start the server with the 'run()' method
