96647bbed99fde71555a9fb2f1dc477da3b3865a
Otavio Carneiro <otaviocsantos@gmail.com>
2014-06-19 18:47:22 -0300

    Organização do projeto

--- python/app.py
+++ python/app.py
@@ -7,11 +7,7 @@ app = Flask(__name__)
 # use decorators to link the function to a url
 @app.route('/')
 def home():
-    return "Hello, World!"  # return a string
-
-@app.route('/welcome')
-def welcome():
-    return render_template('welcome.html')  # render a template
+    return render_template('welcome.html') 
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
